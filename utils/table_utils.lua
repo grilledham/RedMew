43c8fe7c265d81662a9fa5df24a62faff963024d
grilledham <jrdgillham@gmail.com>
2018-10-10 23:39:41 +0100

    updates

--- utils/table_utils.lua
+++ utils/table_utils.lua
@@ -1,30 +1,8 @@
 local Iterable = {}
 Iterable.__index = Iterable
 
-function Iterable.new(tbl)
-    --[[  local function get_iterator()
-        local key, value
-        return function()
-            key, value = next(tbl, key)
-            return key, value
-        end
-    end ]]
-    return setmetatable(tbl or {}, Iterable)
-end
-
-function Iterable.as_iterable(tbl)
-    local function get_iterator()
-        local key, value
-        return function()
-            key, value = next(tbl, key)
-            return key, value
-        end
-    end
-    return setmetatable({get_iterator = get_iterator}, Iterable)
-end
-
 function Iterable:to_table()
-    local iterator = self.get_iterator()
+    local iterator = self:get_iterator()
     local result = {}
     local key, value = iterator()
     while key ~= nil do
@@ -37,7 +15,7 @@ end
 
 function Iterable:filter(predicate)
     local function get_iterator()
-        local iter = self.get_iterator()
+        local iter = self:get_iterator()
         local key, value
 
         return function()
@@ -56,7 +34,7 @@ end
 
 function Iterable:map(project)
     local function get_iterator()
-        local iter = self.get_iterator()
+        local iter = self:get_iterator()
         return function()
             local key, value = iter()
             if key ~= nil then
@@ -69,7 +47,7 @@ function Iterable:map(project)
 end
 
 function Iterable:foreach(action)
-    for k, v in self.get_iterator() do
+    for k, v in self:get_iterator() do
         action(k, v)
     end
 end
@@ -78,22 +56,16 @@ table.__index = table
 setmetatable(table, Iterable)
 
 function table.new(tbl)
-    if not tbl then
-        tbl = {}
-    end
-
-    tbl.get_iterator = function()
-        return Iterable.as_iterable(tbl).get_iterator()
-    end
-
-    return setmetatable(tbl, table)
+    return setmetatable(tbl or {}, table)
 end
 
-function table.get_iterator()
-    return nil
+function table:get_iterator()
+    local key, value
+    return function()
+        key, value = next(self, key)
+        return key, value
+    end
 end
-
-local t = table.new({1, 2, 3})
 
 local function predicate(k, v)
     return v < 3
@@ -107,9 +79,16 @@ local function action(k, v)
     print(k .. ' : ' .. v)
 end
 
+local t = table.new({1, 2, 3})
+
 local query1 = t:filter(predicate)
 query1:foreach(action)
---query1:foreach(action)
---query1:map(project):foreach(action)
+query1:foreach(action)
+query1:map(project):foreach(action)
+local t2 = t:to_table()
+
+for k, v in pairs(t2) do
+    action(k, v)
+end
 
 return Iterable
